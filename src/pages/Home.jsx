ef5f8df81c6a502118741290b113a7e033b8b135
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-08-05 18:24:56 +0000

    keep on  :fire:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
 				<div className="container d-flex justify-content-start">
 					<div className=" my-3 me-3">
 						<div className="card" style={{ width: '18rem' }}>
-							<img src="..." className="card-img-top" alt="..."></img>
+							<img src="https://i.pravatar.cc" className="card-img-top" alt="..."></img>
 							<div className="card-body">
 								<h5 className="card-title">Card title</h5>
 								<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -34,7 +34,7 @@ export const Home = () => {
 				<div className="container d-flex justify-content-start">
 					<div className=" my-3 me-3">
 						<div className="card" style={{ width: '18rem' }}>
-							<img src="..." className="card-img-top" alt="..."></img>
+							<img src="https://i.pravatar.cc/300" className="card-img-top" alt="..."></img>
 							<div className="card-body">
 								<h5 className="card-title">Card title</h5>
 								<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
