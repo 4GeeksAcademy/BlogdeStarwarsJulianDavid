228726d6c682708f26d5509e76b2fce891390fe5
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-08-06 03:35:39 +0000

    Fetch Planets ok, :tada:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,60 +7,56 @@ export const Home = () => {
 	const { store, dispatch } = useGlobalReducer()
 
 	const [planets, setPlanets] = useState([])
-	const [planetsDetails, setPlanetsDetails] = useState([])
+	// const [planetsDetails, setPlanetsDetails] = useState([])
 
 
-	const apiUrlBase = 'https://www.swapi.tech/api/'
 	const apiUrlPlanet = 'https://www.swapi.tech/api/planets'
-	const apiUrlPlanetIndividual = 'https://www.swapi.tech/api/planets/1'
 
-	async function fetchPlanetDetails() {
-		try {
-			const response = await fetch(apiUrlPlanetIndividual)
 
-		if(!response.ok){
-			throw new Error(`HTTP error! status: ${response.status}`);
-		}	
-		const data = await response.json();
-		setPlanetsDetails(data.result.properties)
-		console.log(data.result.properties)
-		return data.result.properties
-		}catch(error){
-			console.error(`Error fetching data: `, error)
-			throw error;
-		}
-		
+	async function fetchPlanetDetails(url) {
+		const response = await fetch(url);
+		return await response.json();
 	}
-	
+
 	async function fetchDataPlanets() {
-		try{
+		try {
 			const response = await fetch(apiUrlPlanet);
 
-		if(!response.ok) {
-			throw new Error(`HTTP Error! status: ${response.status}`);
-		}
+			if (!response.ok) {
+				throw new Error(`HTTP Error! status: ${response.status}`);
+			}
 
-		const data = await response.json();
-		setPlanets(data.results)
-		console.log(data.results)
-		return data.results;
-		}catch(error){
+			const data = await response.json();
+
+			const planetsWithDetails = await Promise.all(
+				data.results.map(async planet => {
+					const details = await fetchPlanetDetails(planet.url);
+
+					return {
+						...planet,
+						details: details.result.properties
+					};
+				})
+			);
+
+			setPlanets(planetsWithDetails);
+
+		} catch (error) {
 			console.error('Error fetching data: ', error);
 			throw error;
 		}
 	}
-	useEffect(()=>{
+	useEffect(() => {
 		fetchDataPlanets();
-		fetchPlanetDetails();
 	}, []);
 
 	return (
 		<div>
 			<div className="text-start mt-5 container">
-				<h1>Characters</h1>
-				<div className="container d-flex justify-content-start">
+				<h1>People</h1>
+				<div className="d-flex flex-row overflow-auto py-3">
 					<div className=" my-3 me-3">
-						<div className="card" style={{ width: '18rem' }}>
+						<div className="card h-100 d-flex flex-column" style={{ width: '18rem' }}>
 							<img src="https://i.pravatar.cc" className="card-img-top" alt="..."></img>
 							<div className="card-body">
 								<h5 className="card-title">Card title</h5>
@@ -73,25 +69,28 @@ export const Home = () => {
 					</div>
 				</div>
 			</div>
-			<div className="text-start mt-5 container">
+			
+			<div className="text-start mt-5 container ">
 				<h1>Star Wars Planet</h1>
-				<div className="container d-flex justify-content-start flex-wrap">
+				<div className="d-flex flex-row overflow-auto py-3">
 					{planets.map(planet => (
 
-					<div key={planet.uid} className=" my-3 me-3">
-						<div className="card" style={{ width: '18rem' }}>
-							<img src="https://i.pravatar.cc/300" className="card-img-top" alt={planet.name}></img>
-							<div className="card-body">
-								<h5 className="card-title">{planet.name}</h5>
-								<h6> Population: {}</h6>
-								<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								<Link to="" >
-									<button type="button" className="btn btn-primary">Learn more</button>
-								</Link>
+						<div key={planet.uid} className=" my-3 me-3">
+							<div className="card h-100 d-flex flex-column" style={{ width: '18rem' }}>
+								<img src="https://i.pravatar.cc/300" className="card-img-top" alt={planet.name}></img>
+								<div className="card-body h-100 d-flex flex-column">
+									<div className="mt-auto">
+										<h5 className="card-title">{planet.name}</h5>
+										<h6> Population: {planet.details.population} </h6>
+										<h6> Terrain: {planet.details.terrain} </h6>
+										<h6> Climate: {planet.details.climate} </h6>
+										<Link to="" >
+											<button type="button" className="btn btn-primary">Learn more</button>
+										</Link>
+									</div>	
+								</div>
 							</div>
 						</div>
-					</div>
-
 					))}
 				</div>
 			</div>
